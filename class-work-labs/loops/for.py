bcc34e34a2fc13bd88ae302e95d328af1dab7da7
hanzlikhan <khangormani79@gmail.com>
2025-04-21 23:37:38 +0500

    init

---
+++ class-work-labs/loops/for.py
@@ -0,0 +1,27 @@
+# for loop 
+
+# print list of string using for loop 
+language = ['c++','python','java']
+for str in language:
+    print(str)
+
+print(language)
+
+# print string character by character using for loop 
+name = 'palwasha'
+for i in name:
+    print(i)
+
+
+
+#  count even number between 1 and 20
+
+count = 0
+for i in range(1,20):
+    if i % 2==0:
+        count = count + 1
+print(count)
+
+
+
+
