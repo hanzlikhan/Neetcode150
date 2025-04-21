31b63874125577c8c9e43577de89c279c996c155
hanzlikhan <khangormani79@gmail.com>
2025-04-21 23:37:29 +0500

    init

---
+++ 4_group_anagram/group_anagram.py
@@ -0,0 +1,17 @@
+from typing import List
+from collections import defaultdict
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    anagram_map = defaultdict(list)  # default value is an empty list
+
+    for word in strs:
+        # Sort the word to find its "signature"
+        sorted_word = ''.join(sorted(word))
+        # Group the word using the sorted form
+        anagram_map[sorted_word].append(word)
+
+    # Return only the grouped values
+    return list(anagram_map.values())
+
+# Example
+print(groupAnagrams(["act", "pots", "tops", "cat", "stop", "hat"]))
