c85b3cf47f68071d98f012616a9e3d4fbb53d94e
Muhammad Hanzla <146532605+hanzlikhan@users.noreply.github.com>
2025-04-28 23:33:44 +0500

    Update group_anagram.py

--- 4_group_anagram/group_anagram.py
+++ 4_group_anagram/group_anagram.py
@@ -5,7 +5,7 @@ def groupAnagrams(strs: List[str]) -> List[List[str]]:
     anagram_map = defaultdict(list)  # default value is an empty list
 
     for word in strs:
-        # Sort the word to find its "signature"
+        # Sort word to find its "signature"
         sorted_word = ''.join(sorted(word))
         # Group the word using the sorted form
         anagram_map[sorted_word].append(word)
