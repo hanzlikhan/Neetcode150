6c4c5180cdb7dd69c47e43bcd71d8c06fa4b9765
hanzlikhan <khangormani79@gmail.com>
2025-05-10 21:59:05 +0500

    nint

---
+++ car-fleet/loop.py
@@ -0,0 +1,47 @@
+def main():
+    
+	x = print("Hello, World!") # 1
+	print(x) # 2
+    # number = 0  # value store  
+    # while (number < 10): # number is greater than or equal  to 1
+    #     # number = 10
+    #     # number = 9
+    #     print("the number is:", number) # this will print 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+    #     print("I'm going to count to three!")
+    #     for i in range(3):
+    #         # Remember range() starts at 0
+    #         print(i + 1) # every while loop will print 1, 2, 3
+    #         print("Blast off!") 
+    #     number += 1
+    # print("loop ended") # this will print only once after the while loop is done
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# def main():
+# 	count = 0
+# 	for i in range(5):
+# 		for j in range(10):
+# 			print("Hello, World!")
+# 			count += 1
+# 	print("Count is:", count)
+
+# if __name__ == "__main__":
+# 	main()
+
+	# print("Pop Quiz! How many continents are there on Earth?") # 1
+	# answer = int(input())   # 2
+	# while(answer != 7):   # 3
+	# 	print("Not quite, guess again!") # 4
+	# 	answer = int(input()) # 5
+	# 	print("That's right! There are seven continents on Earth!") # 6
+	# print("our loop was ended") # 6
+
+    
+
+if __name__ == '__main__':
+    main()
+    
