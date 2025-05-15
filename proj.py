1bdf88f2db6c2cdb775c4700db3f8cff2eae931d
hanzlikhan <khangormani79@gmail.com>
2025-05-15 21:51:43 +0500

    init

--- proj.py
+++ proj.py
@@ -71,6 +71,6 @@ def main():
         if again != "yes":
             print("👋 Thanks for practicing! Goodbye.")
             break
-# 
+# it is the main function that runs the program
 if __name__ == "__main__":
     main()
