1911ea3b775d0d5d2de7b12d7e6dd0bf3fa49da7
Muhammad Hanzla <146532605+hanzlikhan@users.noreply.github.com>
2025-05-21 23:18:39 +0500

    Update group_anagram.py

--- 4_group_anagram/group_anagram.py
+++ 4_group_anagram/group_anagram.py
@@ -1,6 +1,6 @@
 from typing import List
 from collections import defaultdict
-# group angram leetcode question 
+# group anagram leetcode question 
 def groupAnagrams(strs: List[str]) -> List[List[str]]:
     anagram_map = defaultdict(list)  # default value is an empty list
 
